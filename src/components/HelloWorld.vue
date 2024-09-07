4c6edededd67ccc114712bda667cf6fab2ba81a9
abhishek9111 <42829419+Abhishek9111@users.noreply.github.com>
2024-09-07 18:49:31 +0530

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="text-red-700">Boiler plate code test</div>
+</template>
+
+<style scoped></style>
